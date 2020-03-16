59b51cf769405f271a9c9568ec8c59e4105cd19f
Ansh Saxena <a.saxena@se18.qmul.ac.uk>
2020-03-16 11:00:06 +0000

    Use 'Homepage', 'addFutureActivity' and 'addFood' java classes and obviously the 'MainActivity' under the com.hfs.ui folder.

--- app/src/main/java/com/hfs/ui/MainActivity.java
+++ app/src/main/java/com/hfs/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hfs.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,17 +12,40 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button1; //Button for setting future activities from homepage
+    private Button button2; //Button for adding food items from homepage
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Associating button1&2 with relevant button id's
+        button1 = (Button) findViewById(R.id.setFutureActivity);
+        button2 = (Button) findViewById(R.id.setFoodItem);
+
+        //Setting the action that needs to be performed after specific button click from homepage
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openSetFutureActivity();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openSetFoodItem();
+            }
+        });
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
+/*
+        FloatingActionButton = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,6 +53,17 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+ */
+    }
+
+    public void openSetFutureActivity() {
+        Intent intent = new Intent(this, addFutureActivity.class);
+        startActivity(intent);
+    }
+
+    public void openSetFoodItem() {
+        Intent intent = new Intent(this, addFood.class);
+        startActivity(intent);
     }
 
     @Override
