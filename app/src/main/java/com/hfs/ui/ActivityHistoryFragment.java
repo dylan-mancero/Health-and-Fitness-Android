f41442cb5e3832cdd2cc0b725fb0ea3576c8d8e4
Ansh Saxena <a.saxena@se18.qmul.ac.uk>
2020-03-17 17:25:40 +0000

    Added basic hard coded list view for 'Activity History' navigation.

--- app/src/main/java/com/hfs/ui/ActivityHistoryFragment.java
+++ app/src/main/java/com/hfs/ui/ActivityHistoryFragment.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -14,7 +19,7 @@ import android.view.ViewGroup;
  */
 public class ActivityHistoryFragment extends Fragment {
 
-    private static final String TAG = "MainActivity";
+    private static final String TAG = "ActivityHistory";
 
     public ActivityHistoryFragment() {
         // Required empty public constructor
@@ -24,7 +29,21 @@ public class ActivityHistoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activity_history, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_activity_history, container, false);
+
+        ListView list = (ListView) view.findViewById(R.id.activity_history_list);
+        Log.d(TAG, "onCreateView: Started.");
+
+        ArrayList<String> activities = new ArrayList<>();
+        activities.add("Cycling");
+        activities.add("Running");
+        activities.add("Jogging");
+        activities.add("Hiking");
+        activities.add("Walking");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this.getContext(), android.R.layout.simple_list_item_1, activities);
+        list.setAdapter(adapter);
+        return view;
     }
 }
