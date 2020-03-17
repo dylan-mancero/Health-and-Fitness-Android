880cf218319d772d3aa7f10fd8af71cff49e2f2a
Ansh Saxena <a.saxena@se18.qmul.ac.uk>
2020-03-17 01:42:39 +0000

    Navigation bar at the bottom now correctly should display the relevant fragment.

--- app/src/main/java/com/hfs/ui/MainActivity.java
+++ app/src/main/java/com/hfs/ui/MainActivity.java
@@ -3,25 +3,72 @@ package com.hfs.ui;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
     private Button button1; //Button for setting future activities from homepage
     private Button button2; //Button for adding food items from homepage
 
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+
+    private HomeFragment homeFragment;
+    private FoodHistoryFragment foodHistoryFragment;
+    private ActivityHistoryFragment activityHistoryFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+
+        homeFragment = new HomeFragment();
+        foodHistoryFragment = new FoodHistoryFragment();
+        activityHistoryFragment = new ActivityHistoryFragment();
+
+        setFragment(homeFragment);
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.nav_home :
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(homeFragment);
+                        return true;
+
+                    case R.id.nav_foodHistory :
+                        mMainNav.setItemBackgroundResource(R.color.colorAccent);
+                        setFragment(foodHistoryFragment);
+                        return true;
+
+                    case R.id.nav_activityHistory :
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimaryDark);
+                        setFragment(activityHistoryFragment);
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
 
         //Associating button1&2 with relevant button id's
         button1 = findViewById(R.id.setFutureActivity);
@@ -34,13 +81,20 @@ public class MainActivity extends AppCompatActivity {
                 openSetFutureActivity();
             }
         });
-
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
                 openSetFoodItem();
             }
         });
+
+    }
+
+    private void setFragment(HomeFragment fragment) {
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
 
     }
 
